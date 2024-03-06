50758654e6772f4f6a2000ac515e3778b9fedcc7
= <mishevski0800@gmail.com>
2024-03-06 20:47:55 +0100

    Added agree terms app in c

---
+++ week 1/agree/agree.c
@@ -0,0 +1,15 @@
+#include "../lib/cs50.h"
+#include <stdio.h>
+
+int main(void){
+    char c = get_char("Do you agree? ");
+
+    if(c == 'y' || c == 'Y'){
+        printf("Agreed.\n");
+    }else if(c == 'n' || c == 'N'){
+        printf("Not agreed.\n");
+    }else{
+        printf("Wrong input.\n");
+        main();
+    }
+}
