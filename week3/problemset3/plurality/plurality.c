3532ce3f92882379a0ba2e5257d3cbe4387a883c
= <mishevski0800@gmail.com>
2024-03-16 12:23:23 +0100

    Added fixes for plurality task of problem set 3

--- week3/problemset3/plurality/plurality.c
+++ week3/problemset3/plurality/plurality.c
@@ -65,6 +65,14 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
+    for(int i = 0; i < candidate_count; i++)
+    {
+        if(strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes += 1;
+            return true;
+        }
+    }
     // TODO
     return false;
 }
@@ -72,6 +80,25 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
+
+    int max_votes = 0;
+
+    for(int i = 0; i < candidate_count; i++)
+    {
+        if(candidates[i].votes > max_votes)
+        {
+            max_votes = candidates[i].votes;
+        }
+    }
+
+    for(int i = 0; i < candidate_count; i++)
+    {
+        if(candidates[i].votes == max_votes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
+
     // TODO
     return;
 }
