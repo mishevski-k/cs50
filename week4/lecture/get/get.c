318f01cd1c45926ee4fc31e7ffc92321e8af2fda
= <mishevski0800@gmail.com>
2024-03-17 13:40:20 +0100

    Added testing c file to test gettting input from user from different data types

---
+++ week4/lecture/get/get.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // int n;
+    // printf("n: ");
+    // scanf("%i", &n);
+    // printf("n: %i\n", n);
+
+    char s[4];
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
+}
