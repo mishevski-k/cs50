6923b7b6641c6b96e74542ec278bedd34c1f3afa
= <mishevski0800@gmail.com>
2024-03-23 15:36:07 +0100

    Refactored calculator to include more error handling for number inputs

--- week6/lecture/calculator/calculator.py
+++ week6/lecture/calculator/calculator.py
@@ -1,6 +1,21 @@
-import cs50
+# import cs50
 
-x = cs50.get_int("x: ")
-y = cs50.get_int("y: ")
+# x = cs50.get_int("x: ")
+# y = cs50.get_int("y: ")
 
-print(x + y)
+# print(x + y)
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Not an integer")
+
+def main():
+    x = get_int("x: ")
+    y = get_int("y: ")
+
+    print(x + y)
+
+main()
