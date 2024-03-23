9d924d5ef6e60235134432e7072c2e54cd51bbce
= <mishevski0800@gmail.com>
2024-03-23 16:05:22 +0100

    Improved phonebook app to use python dictionaries

--- week6/lecture/phonebook/phonebook.py
+++ week6/lecture/phonebook/phonebook.py
@@ -1,6 +1,6 @@
-names = ["Carter", "David", "John"]
+# names = ["Carter", "David", "John"]
 
-name = input("Name: ")
+# name = input("Name: ")
 
 # for n in names:
 #     if name == n:
@@ -9,7 +9,39 @@ name = input("Name: ")
 # else:
 #     print("Not found")
 
-if name in names:
-    print("Found")
+# if name in names:
+#     print("Found")
+# else:
+#     print("Not found")
+
+# people = [
+#     {
+#         "name": "Carter", "number": "+1-617-495-1000"
+#     },
+#     {
+#         "name": "David", "number": "+1-617-495-1000"
+#     },
+#     {
+#         "name": "John", "number": "+1-949-468-2750"
+#     },
+# ]
+
+people = {
+    "Carter": "+1-617-495-1000",
+    "David": "+1-617-495-1000",
+    "John": "+1-949-468-2750",
+}
+
+name = input("Names: ")
+
+# for person in people:
+#     if person["name"] == name:
+#         print(f"Found {person['number']}")
+#         break
+# else:
+#     print("Not found")
+
+if name in people:
+    print(f"Found {people[name]}")
 else:
     print("Not found")
