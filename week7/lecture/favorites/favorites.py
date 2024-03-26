5fe3c6662f6cfd71b93f2af972894155e44045f6
= <mishevski0800@gmail.com>
2024-03-26 22:02:25 +0100

    Added python app to fetch csv file and print out favorite problem from input

---
+++ week7/lecture/favorites/favorites.py
@@ -0,0 +1,17 @@
+import csv
+
+from collections import Counter
+
+def main():
+    with open("favorites.csv", "r") as file:
+        reader = csv.DictReader(file)
+        counts = Counter()
+
+        for row in reader:
+            favorite = row["problem"]
+            counts[favorite] += 1
+
+    favorite = input("Favorite: ")
+    print(f"{favorite}: {counts[favorite]}")
+
+main()
